e08dda85f00b1690bf9d597147b4cd368b06161e
Hossain, Towseef <towseef_hossain@intuit.com>
2020-08-30 03:24:17 -0400

    Making queries safe to execute

--- src/App.js
+++ src/App.js
@@ -4,9 +4,20 @@ import './App.css';
 import SearchBar from './components/SearchBar'
 import SearchResults from './components/SearchResults'
 import Nominations from './components/Nominations'
+import {Banner} from '@shopify/polaris'
+import Alert from './components/Alert';
 
 const API = "http://www.omdbapi.com/?apikey=b83a24c7"
 
+const noResults = [
+      {
+          "Title": "Bad Request Error",
+          "Year": "Please type in a valid string query",
+          "imdbID": "404",
+          "Type": "movie",
+          "Poster": "https://m.media-amazon.com/images/M/MV5BMTI5Mjg1MzM4NF5BMl5BanBnXkFtZTcwNTAyNzUzMw@@._V1_SX300.jpg"
+      },
+  ]
 
 class App extends React.Component {
   constructor(){
@@ -20,12 +31,28 @@ class App extends React.Component {
     this.addNomination = this.addNomination.bind(this)
     this.removeNomination = this.removeNomination.bind(this)
     this.shouldDisableAddButton = this.shouldDisableAddButton.bind(this)
+    this.shouldShowAlert = this.shouldShowAlert.bind(this)
+    this.setResults = this.setResults.bind(this)
   }
 
   setResults(results) {
-    fetch(API + "&s=" + results + "&type=movie")
+    fetch(API + "&s=" +  escape(results) + "&type=movie")
     .then(response => response.json())
+    .then(response => {
+      if (response.Response == "False") {
+        throw("Bad Request Error")
+      }
+    })
     .then(data => this.setState({searchresults: data.Search}))
+    .catch(error => {
+      console.log(error)
+      this.setState(
+        {
+          searchresults : noResults
+        }
+        )
+      }
+        )
   }
 
   addNomination(nomination) {
@@ -44,7 +71,11 @@ class App extends React.Component {
   }
 
   shouldDisableAddButton(item) {
-    return ((this.state.nominations.indexOf(item) > -1) || (this.state.nominations.length >= 5))
+    return ((this.state.nominations.indexOf(item) > -1) || (this.state.nominations.length >= 5) || item.imdbID == "404")
+  }
+
+  shouldShowAlert() {
+    return this.state.nominations.length >= 5
   }
 
   componentDidMount() {
@@ -61,6 +92,7 @@ class App extends React.Component {
           <SearchResults results={this.state.searchresults} addNomination={this.addNomination} shouldDisableAddButton={this.shouldDisableAddButton}/>
           <Nominations nominations={this.state.nominations} removeNomination={this.removeNomination}/>
         </div>
+        <Alert shouldShowAlert={this.state.nominations.length}/>
       </div>
     );
   }
---
+++ src/components/Alert.js
@@ -0,0 +1,52 @@
+import React, { useCallback, useState } from 'react';
+import { Button, TextContainer, Modal } from '@shopify/polaris';
+
+export default class Alert extends React.Component {
+    constructor(){
+        super()
+        this.state = {
+            alreadyShown : false,
+            open : false
+        }
+    }
+
+    componentDidUpdate(){
+        if ((this.props.shouldShowAlert === 5) && (this.state.open == false) && (this.state.alreadyShown == false)) {
+            this.setState({
+                open: true,
+                alreadyShown: true
+            })
+        }
+        else if ((this.props.shouldShowAlert < 5) && (this.state.alreadyShown == true)) {
+            this.setState({
+                open: false,
+                alreadyShown: false
+            })
+        }
+    }
+
+    handleChange = () => {
+        this.setState({
+            open: false
+        })
+    }
+
+    render() {
+        return <div style={{ height: '500px' }}>
+            <Modal onClose={this.handleChange} title="Nomination Selections Complete" primaryAction={{
+                content: 'Close',
+                onAction: this.handleChange
+            }} open={this.state.open}>
+                <Modal.Section>
+                    <TextContainer>
+                        <p>
+                            You've added 5 nominations! You can now submit your nominations and see the leaderboards, 
+                            or go back and change your selections.
+            </p>
+                    </TextContainer>
+                </Modal.Section>
+            </Modal>
+        </div>;
+    }
+
+}
