35b2817d2fa76e5990a8e6f61555c85b994cc371
M Towseef Hossain <towseef.22aug@gmail.com>
2020-09-01 19:33:17 -0400

    Removing unnecessary files

--- src/App.js
+++ src/App.js
@@ -1,31 +1,29 @@
-import React, {setState} from 'react';
-import logo from './logo.svg';
+import React, { setState } from 'react';
 import './App.css';
 import SearchBar from './components/SearchBar'
 import SearchResults from './components/SearchResults'
 import Nominations from './components/Nominations'
-import {Banner} from '@shopify/polaris'
 import Alert from './components/Alert';
 
 const API = "http://www.omdbapi.com/?apikey=b83a24c7"
 
 const noResults = [
-      {
-          "Title": "Bad Request Error",
-          "Year": "Please type in a valid string query",
-          "imdbID": "404",
-          "Type": "movie",
-          "Poster": "https://m.media-amazon.com/images/M/MV5BMTI5Mjg1MzM4NF5BMl5BanBnXkFtZTcwNTAyNzUzMw@@._V1_SX300.jpg"
-      },
-  ]
+  {
+    "Title": "Bad Request Error",
+    "Year": "Please type in a valid string query",
+    "imdbID": "404",
+    "Type": "movie",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BMTI5Mjg1MzM4NF5BMl5BanBnXkFtZTcwNTAyNzUzMw@@._V1_SX300.jpg"
+  },
+]
 
 class App extends React.Component {
-  constructor(){
+  constructor() {
     super();
 
     this.state = {
-      searchresults : [],
-      nominations : []
+      searchresults: [],
+      nominations: []
     }
     this.setResults = this.setResults.bind(this)
     this.addNomination = this.addNomination.bind(this)
@@ -36,23 +34,23 @@ class App extends React.Component {
   }
 
   setResults(results) {
-    fetch(API + "&s=" +  escape(results) + "&type=movie")
-    .then(response => response.json())
-    .then(response => {
-      if (response.Response == "False") {
-        throw("Bad Request Error")
-      }
-    })
-    .then(data => this.setState({searchresults: data.Search}))
-    .catch(error => {
-      console.log(error)
-      this.setState(
-        {
-          searchresults : noResults
+    fetch(API + "&s=" + escape(results) + "&type=movie")
+      .then(response => response.json())
+      .then(response => {
+        if (response.Response == "False") {
+          throw ("Bad Request Error")
         }
+      })
+      .then(data => this.setState({ searchresults: data.Search }))
+      .catch(error => {
+        console.log(error)
+        this.setState(
+          {
+            searchresults: noResults
+          }
         )
       }
-        )
+      )
   }
 
   addNomination(nomination) {
@@ -67,7 +65,7 @@ class App extends React.Component {
     if (index > -1) {
       checked.splice(index, 1);
     }
-    this.setState({nominations: checked});
+    this.setState({ nominations: checked });
   }
 
   shouldDisableAddButton(item) {
@@ -80,23 +78,23 @@ class App extends React.Component {
 
   componentDidMount() {
     fetch(API + "&s=Rambo&type=movie")
-    .then(response => response.json())
-    .then(data => this.setState({searchresults: data.Search}))
+      .then(response => response.json())
+      .then(data => this.setState({ searchresults: data.Search }))
   }
 
   render() {
     return (
       <div className="App" >
-        <SearchBar setResults={this.setResults}/>
+        <SearchBar setResults={this.setResults} />
         <div className="content-wrap">
-          <SearchResults results={this.state.searchresults} addNomination={this.addNomination} shouldDisableAddButton={this.shouldDisableAddButton}/>
-          <Nominations nominations={this.state.nominations} removeNomination={this.removeNomination}/>
+          <SearchResults results={this.state.searchresults} addNomination={this.addNomination} shouldDisableAddButton={this.shouldDisableAddButton} />
+          <Nominations nominations={this.state.nominations} removeNomination={this.removeNomination} />
         </div>
-        <Alert shouldShowAlert={this.state.nominations.length}/>
+        <Alert shouldShowAlert={this.state.nominations.length} />
       </div>
     );
   }
-  
+
 }
 
 export default App;
